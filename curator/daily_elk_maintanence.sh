918bddf093afa68cdcbdac4b247a724343a69756
Fedele Mantuano <mantuano.fedele@gmail.com>
2017-12-02 17:39:42 +0100

    Removed not useful files

--- curator/daily_elk_maintanence.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-CONF_FILE=/opt/curator/curator.yml
-DAILY_ACTIONS=/opt/curator/daily_actions.yml
-
-COMMAND=/usr/local/bin/curator
-
-${COMMAND} --config ${CONF_FILE} ${DAILY_ACTIONS}
--- my_init.d/99_spamscope_elasticsearch.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-spamscope-elasticsearch template -p ${SPAMSCOPE_PATH}/conf/templates/spamscope.json -n spamscope
-spamscope-elasticsearch template -p ${SPAMSCOPE_PATH}/conf/templates/commons.json -n commons
-spamscope-elasticsearch replicas
